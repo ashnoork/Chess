5a9fb72f2b09ba9fa41fc3f27c012a37260ee1fe
Ashnoor Kaur Randhawa <88097301+ashnoork@users.noreply.github.com>
2023-12-21 17:36:01 -0500

    Add files via upload

---
+++ level4.cc
@@ -0,0 +1,5 @@
+#include "level4.h"
+#include "board.h"
+
+
+Level4::Level4(Color team): Computer{team} {}
---
+++ level4.h
@@ -0,0 +1,10 @@
+#ifndef __LEVEL4_H__
+#define __LEVEL4_H__
+#include "computer.h"
+
+class Level4: public Computer{
+    public:
+        Level4 (Color team);
+}; 
+
+#endif
